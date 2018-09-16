d09b4fe045061fb9deeec1362b450db9a45bc033
Administrator <Administrator@PC-20160114JFOU>
2018-09-16 12:04:16 +0800

    build

---
+++ src/main/java/com/wuyuan/spring/cloud/weather/service/CityListService.java
@@ -0,0 +1,16 @@
+package com.wuyuan.spring.cloud.weather.service;
+
+import java.util.List;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.wuyuan.spring.cloud.weather.vo.City;
+
+
+@FeignClient("weather-city-server-eureka")
+public interface CityListService {
+
+		@GetMapping("/cities")
+		 List<City>  listCity();
+}
